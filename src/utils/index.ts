e48e14a14b1b019dbb7d37182e296ed2a0216ffa
MapleCyan <849221076@qq.com>
2022-04-28 20:28:01 +0800

    chore: 给useDebounce添加泛型

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -22,7 +22,7 @@ export const useMount = (callback: () => void) => {
   }, []);
 };
 
-export const useDebounce = (value: any, delay?: number) => {
+export const useDebounce = <T>(value: T, delay?: number): T => {
   const [debouncedValue, setDebounceValue] = useState(value);
 
   useEffect(() => {
