e42cddf2b2a1350b53fe6519bcd531791ef3cce3
MapleCyan <849221076@qq.com>
2022-04-29 16:00:12 +0800

    feat: 添加登录注册系统

---
+++ src/unauthenticated-app/login.tsx
@@ -0,0 +1,39 @@
+import { useAuth } from "context/auth-context";
+import { FormEvent } from "react";
+
+export const LoginScreen = () => {
+  const { login, user } = useAuth();
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const username = (event.currentTarget.elements[0] as HTMLInputElement)
+      .value;
+    const password = (event.currentTarget.elements[1] as HTMLInputElement)
+      .value;
+    login({ username, password });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">用户名</label>
+        <input
+          type="text"
+          name="username"
+          id={"username"}
+          autoComplete="username"
+        />
+      </div>
+      <div>
+        <label htmlFor="password">密码</label>
+        <input
+          type="password"
+          name="password"
+          id={"password"}
+          autoComplete="current-password"
+        />
+      </div>
+      <button type="submit">登录</button>
+    </form>
+  );
+};
