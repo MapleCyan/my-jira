2c157ab84506b8a801d6fc936c3bb681b2bc345d
MapleCyan <849221076@qq.com>
2022-05-03 15:36:18 +0800

    style: 使用Row布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { Row } from "components/lib";
 import { useAuth } from "context/auth-context";
 import { ProjectListScreen } from "screens/project-list";
 
@@ -6,8 +7,8 @@ export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <Container>
-      <Header>
-        <HeaderLeft>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
           <h3>logo</h3>
           <h3>项目</h3>
           <h3>用户</h3>
@@ -29,22 +30,10 @@ const Container = styled.div`
   grid-template-rows: 6rem 1fr;
   height: 100vh;
   background-color: aliceblue;
-  grid-template-areas: "header" "main";
 `;
 
 // grid-area 用来给grid子元素起名字
-const Header = styled.header`
-  grid-area: header;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`;
-const HeaderLeft = styled.div`
-  display: flex;
-  align-items: center;
-`;
+const Header = styled(Row)``;
+const HeaderLeft = styled(Row)``;
 const HeaderRight = styled.div``;
-const Main = styled.main`
-  grid-area: main;
-`;
+const Main = styled.main``;
